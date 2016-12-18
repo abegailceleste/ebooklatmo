1455b26dca5a7fad83bcc3d8f7a7cbf8bf3cb145
abegailceleste <abe.gailceleste@gmail.com>
2016-12-18 19:47:53 +0800

    Add files via upload

---
+++ js/process.js
@@ -0,0 +1,253 @@
+
+      var myApp = new Framework7(); 
+      
+      var $$ = Dom7;
+      
+      var mainView = myApp.addView('.view-main', {
+        dynamicNavbar: true
+      });
+      
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook1 = myApp.photoBrowser({
+          photos : [
+               'books/dannythedragon/00.png',
+               'books/dannythedragon/01.png',
+               'books/dannythedragon/02.png',
+               'books/dannythedragon/03.png',
+               'books/dannythedragon/04.png',
+               'books/dannythedragon/05.png',
+               'books/dannythedragon/06.png',
+               'books/dannythedragon/07.png',
+               'books/dannythedragon/08.png',
+               'books/dannythedragon/09.png',
+               'books/dannythedragon/10.png',
+               'books/dannythedragon/11.png',
+          ]
+      });
+      //Open photo browser on click
+      $$('.book1').on('click', function () {
+          myPhotoBrowserBook1.open();
+      });
+
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook2 = myApp.photoBrowser({
+          photos : [
+              'books/finbo/00.png',
+               'books/finbo/01.png',
+               'books/finbo/02.png',
+               'books/finbo/03.png',
+               'books/finbo/04.png',
+               'books/finbo/05.png',
+               'books/finbo/06.png',
+               'books/finbo/07.png',
+               'books/finbo/08.png',
+               'books/finbo/09.png',
+               'books/finbo/10.png',
+              'books/finbo/11.png',
+              'books/finbo/12.png',
+              'books/finbo/13.png',
+              'books/finbo/14.png',
+          ]
+      });
+      //Open photo browser on click
+      $$('.book2').on('click', function () {
+          myPhotoBrowserBook2.open();
+      });
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook3 = myApp.photoBrowser({
+          photos : [
+              'books/ammasbirthday/00.png',
+               'books/ammasbirthday/01.png',
+               'books/ammasbirthday/02.png',
+               'books/ammasbirthday/03.png',
+               'books/ammasbirthday/04.png',
+               'books/ammasbirthday/05.png',
+               'books/ammasbirthday/06.png',
+               'books/ammasbirthday/07.png',
+               'books/ammasbirthday/08.png',
+               'books/ammasbirthday/09.png',
+               'books/ammasbirthday/10.png',
+              'books/ammasbirthday/11.png',
+              'books/ammasbirthday/12.png',
+              'books/ammasbirthday/13.png',
+              'books/ammasbirthday/14.png',
+          ]
+      });
+      //Open photo browser on click
+      $$('.book3').on('click', function () {
+          myPhotoBrowserBook3.open();
+      });
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook4 = myApp.photoBrowser({
+          photos : [
+              'books/danandthefrog/00.png',
+               'books/danandthefrog/01.png',
+               'books/danandthefrog/02.png',
+               'books/danandthefrog/03.png',
+               'books/danandthefrog/04.png',
+               'books/danandthefrog/05.png',
+               'books/danandthefrog/06.png',
+               'books/danandthefrog/07.png',
+               'books/danandthefrog/08.png',
+               'books/danandthefrog/09.png',
+               'books/danandthefrog/10.png',
+              'books/danandthefrog/11.png',
+              'books/danandthefrog/12.png',
+              'books/danandthefrog/13.png',
+              'books/danandthefrog/14.png',
+          ]
+      });
+      //Open photo browser on click
+      $$('.book4').on('click', function () {
+          myPhotoBrowserBook4.open();
+      });
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook5 = myApp.photoBrowser({
+          photos : [
+              'books/fishoutofwater/00.png',
+               'books/fishoutofwater/01.png',
+               'books/fishoutofwater/02.png',
+               'books/fishoutofwater/03.png',
+               'books/fishoutofwater/04.png',
+               'books/fishoutofwater/05.png',
+               'books/fishoutofwater/06.png',
+               'books/fishoutofwater/07.png',
+               'books/fishoutofwater/08.png',
+               'books/fishoutofwater/09.png',
+               'books/fishoutofwater/10.png',
+              'books/fishoutofwater/11.png',
+              'books/fishoutofwater/12.png',
+              'books/fishoutofwater/13.png',
+              'books/fishoutofwater/14.png',
+              'books/fishoutofwater/15.png',
+              'books/fishoutofwater/16.png',
+              'books/fishoutofwater/17.png',
+          ]
+      });
+      //Open photo browser on click
+      $$('.book5').on('click', function () {
+          myPhotoBrowserBook5.open();
+      });
+      
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook6 = myApp.photoBrowser({
+          photos : [
+              'books/thewitchdaughter/00.png',
+               'books/thewitchdaughter/01.png',
+               'books/thewitchdaughter/02.png',
+               'books/thewitchdaughter/03.png',
+               'books/thewitchdaughter/04.png',
+               'books/thewitchdaughter/05.png',
+               'books/thewitchdaughter/06.png',
+               'books/thewitchdaughter/07.png',
+               'books/thewitchdaughter/08.png',
+               'books/thewitchdaughter/09.png',
+               'books/thewitchdaughter/10.png',
+              'books/thewitchdaughter/11.png',
+              'books/thewitchdaughter/12.png',
+              'books/thewitchdaughter/13.png',
+              'books/thewitchdaughter/14.png',
+
+          ]
+      });
+      //Open photo browser on click
+      $$('.book6').on('click', function () {
+          myPhotoBrowserBook6.open();
+      });
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook7 = myApp.photoBrowser({
+          photos : [
+              'books/lumberjack/00.png',
+               'books/lumberjack/01.png',
+               'books/lumberjack/02.png',
+               'books/lumberjack/03.png',
+               'books/lumberjack/04.png',
+               'books/lumberjack/05.png',
+               'books/lumberjack/06.png',
+               'books/lumberjack/07.png',
+               'books/lumberjack/08.png',
+               'books/lumberjack/09.png',
+               'books/lumberjack/10.png',
+              'books/lumberjack/11.png',
+              'books/lumberjack/12.png',
+              'books/lumberjack/13.png',
+              'books/lumberjack/14.png',
+              'books/lumberjack/15.png',
+              'books/lumberjack/16.png',
+
+          ]
+      });
+      //Open photo browser on click
+      $$('.book7').on('click', function () {
+          myPhotoBrowserBook7.open();
+      });
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook8 = myApp.photoBrowser({
+          photos : [
+              'books/whostheprettiest/00.png',
+               'books/whostheprettiest/01.png',
+               'books/whostheprettiest/02.png',
+               'books/whostheprettiest/03.png',
+               'books/whostheprettiest/04.png',
+               'books/whostheprettiest/05.png',
+               'books/whostheprettiest/06.png',
+               'books/whostheprettiest/07.png',
+               'books/whostheprettiest/08.png',
+               'books/whostheprettiest/09.png',
+               'books/whostheprettiest/10.png',
+              'books/whostheprettiest/11.png',
+              'books/whostheprettiest/12.png',
+              'books/whostheprettiest/13.png',
+              'books/whostheprettiest/14.png',
+      
+
+          ]
+      });
+      //Open photo browser on click
+      $$('.book8').on('click', function () {
+          myPhotoBrowserBook8.open();
+      });
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook9 = myApp.photoBrowser({
+          photos : [
+              'books/tylernotailmouse/00.png',
+               'books/tylernotailmouse/01.png',
+               'books/tylernotailmouse/02.png',
+               'books/tylernotailmouse/03.png',
+               'books/tylernotailmouse/04.png',
+               'books/tylernotailmouse/05.png',
+               'books/tylernotailmouse/06.png',
+               'books/tylernotailmouse/07.png',
+               'books/tylernotailmouse/08.png',
+               'books/tylernotailmouse/09.png',
+               'books/tylernotailmouse/10.png',
+          ]
+      });
+      //Open photo browser on click
+      $$('.book9').on('click', function () {
+          myPhotoBrowserBook9.open();
+      });
+
+      /*=== Default standalone ===*/
+      var myPhotoBrowserBook10 = myApp.photoBrowser({
+          photos : [
+              'books/terribletom/00.png',
+               'books/terribletom/01.png',
+               'books/terribletom/02.png',
+               'books/terribletom/03.png',
+               'books/terribletom/04.png',
+               'books/terribletom/05.png',
+          ]
+      });
+      //Open photo browser on click
+      $$('.book10').on('click', function () {
+          myPhotoBrowserBook10.open();
+      });
+
